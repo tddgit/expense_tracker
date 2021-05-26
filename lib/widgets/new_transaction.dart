6a491c66f77f208de3513b1b54944adbb3c479d7
Stas <holidayonmail@gmail.com>
2021-05-26 19:20:10 +0300

    Finished first part of the
    Took 1 hour 43 minutes

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-typedef void CallbackForTransaction(String txTitle, double txAmount);
+typedef void CallbackForTransaction(
+    String txTitle, double txAmount, DateTime chosenDate);
 
 class NewTransaction extends StatefulWidget {
   final CallbackForTransaction addTx;
@@ -13,22 +15,38 @@ class NewTransaction extends StatefulWidget {
 
 class _NewTransactionState extends State<NewTransaction> {
   final titleController = TextEditingController();
-
   final amountController = TextEditingController();
+  late DateTime _selectedDate = DateTime.now();
 
-  void submitData() {
+  void _submitData() {
     try {
       final enteredTitle = titleController.text;
       final enteredAmount = double.parse(amountController.text);
-      if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
         return;
       }
-      widget.addTx(enteredTitle, enteredAmount);
+      widget.addTx(enteredTitle, enteredAmount, _selectedDate);
 
       Navigator.of(context).pop();
     } catch (err) {
       amountController.clear();
     }
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(DateTime.now().year),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
   }
 
   @override
@@ -49,12 +67,31 @@ class _NewTransactionState extends State<NewTransaction> {
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
               keyboardType: TextInputType.numberWithOptions(decimal: true),
-              onSubmitted: (_) => submitData(),
+              onSubmitted: (_) => _submitData(),
             ),
-            FlatButton(
-              child: Text('Add Transaction',
-                  style: TextStyle(color: Colors.purple)),
-              onPressed: submitData,
+            Container(
+              height: 70,
+              child: Row(children: <Widget>[
+                Expanded(
+                  child: Text(
+                    _selectedDate == null
+                        ? 'No Date Chosen'
+                        : 'Picked date : ${DateFormat.yMd().format(_selectedDate)}',
+                  ),
+                ),
+                FlatButton(
+                  child: Text('Choose Date',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  textColor: Theme.of(context).primaryColor,
+                  onPressed: _presentDatePicker,
+                ),
+              ]),
+            ),
+            RaisedButton(
+              child: Text('Add Transaction'),
+              color: Theme.of(context).primaryColor,
+              textColor: Theme.of(context).textTheme.button!.color,
+              onPressed: _submitData,
             )
           ],
         ),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,15 +1,17 @@
 import "package:flutter/material.dart";
 import "package:flutter_expense_tracker/models/transaction.dart";
+import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   late final List<Transaction> transactions;
+  final Function deleteTx;
 
-  TransactionList(this.transactions);
+  TransactionList(this.transactions, this.deleteTx);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 650,
       child: transactions.isEmpty
           ? Column(
               children: [
@@ -32,10 +34,34 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemCount: transactions.length,
               itemBuilder: (ctx, index) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    radius: 3,
-                    child: Text('\$${transactions[index].amount}'),
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: FittedBox(
+                          child: Text('\$${transactions[index].amount}'),
+                        ),
+                      ),
+                    ),
+                    title: Text('${transactions[index].title}',
+                        style: Theme.of(context).textTheme.headline6),
+                    subtitle: Text(
+                      '${DateFormat.yMMMd().format(transactions[index].date)}',
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(
+                        Icons.delete,
+                      ),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () => deleteTx(transactions[index].id),
+                    ),
                   ),
                 );
               },
